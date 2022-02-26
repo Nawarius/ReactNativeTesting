95650f41a1f6c6d8713f53623af8f46802e39761
Artem Ermakov <nawarius1993@gmail.com>
2022-02-26 06:52:42 +0400

    Adds: Setup server, setup correct endpoint

--- client/App.js
+++ client/App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import getServerEndpoint from './src/Network/getServerEndpoint';
 
 export default function App() {
+
+  const [serverValue, setServerValue] = useState()
+  
+  const getServerValue = async () => {
+    try {
+      const response = await fetch(`${getServerEndpoint()}/getValue`)
+      const json = await response.json()
+      setServerValue(json.value)
+      console.log(global.location)
+      //console.log(Platform)
+      //console.log(json.value)
+    } catch (error) {
+      //console.log('Error')
+      //console.error(error)
+    } 
+ }
+
+ useEffect(() => {
+  getServerValue()
+ }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Server Value is: {serverValue} </Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ client/src/Network/getServerEndpoint.js
@@ -0,0 +1,6 @@
+
+const ENDPOINT = 'http://10.0.2.2:4000'
+
+const getServerEndpoint = () => ENDPOINT
+
+export default getServerEndpoint
